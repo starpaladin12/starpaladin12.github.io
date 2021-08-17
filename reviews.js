e73a5e8aa35606a6e7ecdbb39cdd97f3e9d354c9
RJ <ramtinraouf@gmail.com>
2021-08-17 18:44:34 -0400

    changed the order of appending elements into div

--- reviews.js
+++ reviews.js
@@ -39,8 +39,8 @@ function setInfo(contain,title, rate){
     //remove the first list in info to add the next information to the next container
     info.shift();
     //put the title and rate element into the div element contain
-    contain.appendChild(title);
     contain.appendChild(rate);
+    contain.insertBefore(rate, title);
     //put div element into html body
     return document.body.appendChild(contain);
 
