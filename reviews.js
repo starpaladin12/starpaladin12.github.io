670044dbc9a7b2c1fe8b534fa6f60393f7afc863
RJ <ramtinraouf@gmail.com>
2021-08-17 22:43:50 -0400

    added an a element and error with putting a button element inside the a element

--- reviews.js
+++ reviews.js
@@ -7,7 +7,9 @@ createContainer();
 function createContainer(){
     //create a for loop that goes through the number of restaurants in restaurants array
     //set info to the same value as restaurants to change the value but still keep the orginal array in restaurants
-    let info = [["Thai Express",4],["Sansotei Ramen", 5],["World Burger", 3]];
+    let info = [["Thai Express",4],["Lone Star", 5],["World Burger", 3]];
+    //create files to be an array of html files to open 
+    let files = ["ThaiExpress.html","LoneStar.html","WorldBurger.html"];
     for (let i = 0;  i < restaurants.length; i++){
         /*create a div element in js and make it equal to variable contain to hold other elements that will get created 
         let title equal to a p element to hold the name of the restaurant 
@@ -27,12 +29,11 @@ function createContainer(){
         //create id for button to be button plus the value of i 
         click.id = "button"+i;
         //call function setInfo to put information into each element 
-        setInfo(contain, title, rate, info, box, click);
+        setInfo(contain, title, rate, info, box, click, files);
     }
-    openPage();
 }
 //create setInfo to put information from resturants array into the elements I have created
-function setInfo(contain,title, rate, info, box, click){
+function setInfo(contain,title, rate, info, box, click, files){
     for (let num = 0; num < (info[0]).length; num++){
         //check if the first list of info is the first value
         if ((info[0]).indexOf(info[0][num])== 0){
@@ -46,22 +47,18 @@ function setInfo(contain,title, rate, info, box, click){
     }
     //remove the first list in info to add the next information to the next container
     info.shift();
+    //set link to an a element
+    let link = document.createElement("a").setAttribute("herf",files[0]);
+    //remove the first value of files 
     //put the title and rate element into the div element contain
     contain.appendChild(title);
     contain.appendChild(rate);
     //put contain into click and click into box
     click.appendChild(contain);
-    box.appendChild(click);
+    //put click inside the a element
+    link.appendChild(click);
+    box.appendChild(link);
     //put div element into html body
     return document.body.appendChild(box);   
 }
 
-function openPage(){
-    //create a list and have all files to open with openPage inside the array
-    let files  = ["WorldBurger.html", "LoneStar.html", "ThaiExpress.html"];
-    //set each button0,1,2 created to open a new file
-    //loop through the length of restaurant array
-    for (let i = 0; i < files.length; i++){
-        document.getElementById("button"+i).onclick = function(){setInfo};
-    }
-}
