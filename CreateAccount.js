50e0c151020013ad5e6de047883d05ea90f9bf72
RJ <ramtinraouf@gmail.com>
2021-08-18 16:03:21 -0400

    error with saving accounts in all the pages

--- CreateAccount.js
+++ CreateAccount.js
@@ -28,6 +28,5 @@ function signIn(){
     accounts.push([username,password]);
     //print out account sucessfully created
     alert("Accoutn sucessfully created");
-    export default accounts;
     return;
 }
