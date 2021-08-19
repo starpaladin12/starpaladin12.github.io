70a3416e400b27050fa8ba6ec3e560e30a3a8ffa
RJ <ramtinraouf@gmail.com>
2021-08-19 12:28:49 -0400

    create a drop down to show all restaurants you can review

--- WriteReview.js
+++ WriteReview.js
@@ -15,13 +15,26 @@ function reviewForum(){
         return;
     }
     //state reviewBox to store a div element
+    let reviewBox = document.createElement("div");
     //give id review to div element
+    reviewBox.id = "review";
     //state forum to store a forum element
+    let forum = document.createElement("forum");
     //state pick to a select tag that has the options be the restaurant names that are in the website right now
+    let pick = document.createElement("select");
     //give a name of resturants and id of pickReview to select tag
+    pick.setAttribute("name","resturants");
+    pick.id = "pickReview";
     //for loop through resturants array 
-    //state options to = an options tag and set that value and inner html to the first value of each nested array looped through
-    //append options into pick to have all the options tag created be inside select tag
+    for (let i = 0; i < restaurants.length; i++){
+        //state options to = an options tag and set that value and inner html to the first value of each nested array looped through
+       let options = document.createElement("option");
+       options.setAttribute("value",(restaurnats[i])[0]);
+       options.innerHTML = (restaurnats[i])[0];
+        //append options into pick to have all the options tag created be inside select tag
+        pick.appendChild(options);
+    }
+    
     //state ratting to store a input element with number type
     //set the id for ratting to rate
     //state review = to a input tag with type = text
